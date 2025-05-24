2e525cd31db2642309bf257d8b46491328dcf21e
dayanFreitas <dayan.freitas.df@gmail.com>
2025-05-23 22:13:17 -0300

    feat: integra roteiros de mensagens na configuração da extensão e remove definição duplicada no arquivo roadmap.js

--- extension/roadmap.js
+++ extension/roadmap.js
@@ -1,23 +1,3 @@
-const roadmap = {
-    'short.txt': 'short.txt',
-    '2Fast2Furious.txt': '2Fast2Furious.txt',
-    'adelehelloSendScript.txt': 'adelehelloSendScript.txt',
-    'bee_movie.txt': 'bee_movie.txt',
-    'clickTheMovieSendScript.txt': 'clickTheMovieSendScript.txt',
-    'geralDoGremio.txt': 'geralDoGremio.txt',
-    'howToTrainYourDragon.txt': 'howToTrainYourDragon.txt',
-    'martinFierroSendScript.txt': 'martinFierroSendScript.txt',
-    'MusicasPontePreta.txt': 'MusicasPontePreta.txt',
-    'oPaiIOSendScript.txt': 'oPaiIOSendScript.txt',
-    'shrek.txt': 'shrek.txt',
-    'shrekSendScript.txt': 'shrekSendScript.txt',
-    'superMarioBros.txt': 'superMarioBros.txt',
-    'terminator2.txt': 'terminator2.txt',
-    'terminator2SendScript.txt': 'terminator2SendScript.txt',
-    'theSuperMarioBrosSendScript.txt': 'theSuperMarioBrosSendScript.txt',
-    'vidaLokaSendScript.txt': 'vidaLokaSendScript.txt'
-}
-
 function isLocalhost() {
     return false
 }
